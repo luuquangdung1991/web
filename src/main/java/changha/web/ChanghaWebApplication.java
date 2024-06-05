5e9c42502b38f6d7e5810cec19cd4ce05d9263d9
QuangDungLuu <quang-dung.luu@deutschebahn.com>
2024-06-05 11:03:26 +0200

    first commit

---
+++ src/main/java/changha/web/ChanghaWebApplication.java
@@ -0,0 +1,13 @@
+package changha.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChanghaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChanghaWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/changha/web/ChanghaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package changha.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChanghaWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
